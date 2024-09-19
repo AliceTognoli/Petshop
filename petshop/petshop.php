989e9d8e8a867ed42274cf1b1cc7929548192e66
AliceTognoli <161716336+AliceTognoli@users.noreply.github.com>
2024-09-19 11:43:57 -0400

    Add files via upload

---
+++ petshop/petshop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>petshop</title>
+</head>
+<body>
+
+    <div>
+    <h1>Cadastro do Colaborador</h1>
+    <div>
+        <h2>Nome do colaborador</h2>
+
+        <h2>CPF do colaborador</h2>
+
+        <button>Cadastrar</button class="btn-cadastrar">
+
+    </div>
+
+</div>
+
+</body>
+</html>
